de02e2f943c8be87ea41070f75970d48d99e2ffa
Saifulla Shaik <shaiksaifulla21@gmail.com>
2018-05-03 17:38:38 +0530

    run the app and donot create socket

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+async_mode = None
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = False
-socketio = SocketIO(app,engineio_logger=False,log_output=False,async_mode='eventlet')
+# socketio = SocketIO(app, async_mode=async_mode)
 
 @app.route('/')
 def index():
@@ -12,7 +13,7 @@ def index():
     
 @app.route('/test')
 def test():
-    return render_template('test.html')
+    return 'test'
 
 @socketio.on('myevent')
 def test_message(message):
@@ -23,4 +24,4 @@ def test_message(message):
     emit('chat message', message)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
